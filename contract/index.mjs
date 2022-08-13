0c4d4fa2f27281462b70b7471c58864854476fdb
Ogar segun <ogarstephen98@gmail.com>
2022-08-14 00:32:38 +0100

    Bob NFT and Metadata(ipfs) deployed on chain

--- contract/index.mjs
+++ contract/index.mjs
@@ -1,6 +1,6 @@
 import {loadStdlib} from '@reach-sh/stdlib';
 import * as backend from './build/index.main.mjs';
-const stdlib = loadStdlib(process.env);
+const stdlib = loadStdlib(process.env.PRIVATE_KEY);
 
 const startingBalance = stdlib.parseCurrency(100);
 
@@ -12,14 +12,26 @@ console.log('Launching...');
 const ctcAlice = accAlice.contract(backend);
 const ctcBob = accBob.contract(backend, ctcAlice.getInfo());
 
+
+const theNFT= await stdlib.launchToken(accBob, "BlogIt", "BLG", {supply: 1, metadataHash: "https://ipfs.io/ipfs/iQmeLJf1LrF3n6edoR5Dkzvr6gTmPWweHpd2xodUKFqKyHg"})
+const nftParams= {
+  nftId: theNFT.id
+}
+
 console.log('Starting backends...');
 await Promise.all([
   backend.Alice(ctcAlice, {
     ...stdlib.hasRandom,
+    ctcAddress: (info)=>{
+      console.log(`This is the contract Address ${info}`)
+    }
     // implement Alice's interact object here
   }),
   backend.Bob(ctcBob, {
     ...stdlib.hasRandom,
+    Mint: ()=>{
+      return nftParams
+    }
     // implement Bob's interact object here
   }),
 ]);
--- contract/index.rsh
+++ contract/index.rsh
@@ -1,18 +1,32 @@
 'reach 0.1';
 
 export const main = Reach.App(() => {
+  //Alice will deploy the contract
   const A = Participant('Alice', {
     // Specify Alice's interact interface here
+    ctcAddress: Fun([Address], Null)
   });
+  //Bob will be able to mint NFT and publish on-chain while storing the metadata on IPFS
   const B = Participant('Bob', {
-    // Specify Bob's interact interface here
+   Mint: Fun([], Object({
+     nftId: Token
+   })) // Specify Bob's interact interface here
   });
   init();
-  // The first one to publish deploys the contract
   A.publish();
+  const info= getAddress();
   commit();
+  A.only(()=>{
+    interact.ctcAddress(info)
+  })
+  // The first one to publish deploys the contract
+ 
+
   // The second one to publish always attaches
-  B.publish();
+  B.only(()=>{
+    const myNFTId= declassify(interact.Mint());
+  })
+  B.publish(myNFTId);
   commit();
   // write your program here
   exit();
